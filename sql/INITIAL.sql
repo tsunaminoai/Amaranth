2d0ab06fbaa011358a788dab773ac65dff78fdf5
TsunamiNoAi <webmaster@falseblue.com>
2013-02-20 14:14:10 -0500

    fix schema name

--- sql/INITIAL.sql
+++ sql/INITIAL.sql
@@ -2,16 +2,13 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-DROP SCHEMA IF EXISTS `anathama` ;
-CREATE SCHEMA IF NOT EXISTS `anathama` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin ;
-USE `anathama` ;
 
 -- -----------------------------------------------------
--- Table `anathama`.`user`
+-- Table `amaranth`.`user`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `anathama`.`user` ;
+DROP TABLE IF EXISTS `amaranth`.`user` ;
 
-CREATE  TABLE IF NOT EXISTS `anathama`.`user` (
+CREATE  TABLE IF NOT EXISTS `amaranth`.`user` (
   `user_id` INT NOT NULL AUTO_INCREMENT ,
   `user_name` VARCHAR(45) NOT NULL ,
   `password` VARCHAR(32) NOT NULL ,
@@ -25,11 +22,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `anathama`.`character`
+-- Table `amaranth`.`character`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `anathama`.`character` ;
+DROP TABLE IF EXISTS `amaranth`.`character` ;
 
-CREATE  TABLE IF NOT EXISTS `anathama`.`character` (
+CREATE  TABLE IF NOT EXISTS `amaranth`.`character` (
   `char_id` INT NOT NULL AUTO_INCREMENT ,
   `char_name` VARCHAR(45) NOT NULL ,
   `dte_created` DATETIME NOT NULL ,
@@ -39,11 +36,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `anathama`.`user_char_xref`
+-- Table `amaranth`.`user_char_xref`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `anathama`.`user_char_xref` ;
+DROP TABLE IF EXISTS `amaranth`.`user_char_xref` ;
 
-CREATE  TABLE IF NOT EXISTS `anathama`.`user_char_xref` (
+CREATE  TABLE IF NOT EXISTS `amaranth`.`user_char_xref` (
   `user_id` INT NOT NULL ,
   `char_id` INT NOT NULL ,
   PRIMARY KEY (`user_id`, `char_id`) ,
@@ -51,17 +48,16 @@ CREATE  TABLE IF NOT EXISTS `anathama`.`user_char_xref` (
   INDEX `char_id_idx` (`char_id` ASC) ,
   CONSTRAINT `user_id`
     FOREIGN KEY (`user_id` )
-    REFERENCES `anathama`.`user` (`user_id` )
+    REFERENCES `amaranth`.`user` (`user_id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `char_id`
     FOREIGN KEY (`char_id` )
-    REFERENCES `anathama`.`character` (`char_id` )
+    REFERENCES `amaranth`.`character` (`char_id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-USE `anathama` ;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
